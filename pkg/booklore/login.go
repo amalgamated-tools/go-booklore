faf5aab44680bd76ed4d66c02aab8e6c32645ef3
Patrick Veverka <veverkap@users.noreply.github.com>
2026-02-01 23:43:03 -0500

    split into files

---
+++ pkg/booklore/login.go
@@ -0,0 +1,47 @@
+package booklore
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+type Credentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func (c *Client) LoginWithCredentials(ctx context.Context, creds Credentials) error {
+	resp, err := c.LoginUser(
+		ctx,
+		LoginUserJSONRequestBody{
+			Username: StrToPtr(creds.Username),
+			Password: StrToPtr(creds.Password),
+		},
+	)
+	if err != nil {
+		return err
+	}
+	response, err := ParseLoginUserResponse(resp)
+	if err != nil {
+		return err
+	}
+	if response.JSON200 == nil {
+		return errors.New("unexpected response from server")
+	}
+	token, ok := (*response.JSON200)["refreshToken"]
+	if !ok {
+		return errors.New("no refresh token in response")
+	}
+	tokenFunc := func(ctx context.Context, req *http.Request) error {
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+		return nil
+	}
+	c.RequestEditors = append(c.RequestEditors, tokenFunc)
+	return nil
+}
+
+func (c *Client) ClientWithResponses() ClientWithResponses {
+	return ClientWithResponses{c}
+}
